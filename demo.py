759a23c329062eb84dded0fd96c0c42d61ad8e79
ws260621690 <william.starnaud@gmail.com>
2018-02-19 12:00:51 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,28 +4,27 @@ from model import *
 file, _ = load_audio("/Users/williamst-arnaud/U de M/MUS3325/msynth/03 Plimsoll Punks.wav", sample_rate=44100)
 
 # Compute the mu-law of the file to restrict the number of possible values. Here we use 8-bit mu-law
-file = mu_law(file, 8)[:1024]
+file = mu_law(file, 255)[:1024]
 
 shape = file.shape
 
 #Reshape input to be used by dilated convolution layer
 file = file.reshape([1, 1, shape[0], 1])
 
-#Ignore this part for now. Problem with datatypes not recognized by Placeholder ???????
-targets = np.floor(file * 256)
-targets = tf.one_hot(targets, depth=256)
-
-
-targets = tf.reshape(targets, shape=[1, 1, 1024, 256])
+# get probability distribution for the labels. Used in cross-entropy loss during training
+targets = get_labels(file, 256)
 
 
 # Use these instead for the feed_dict to test behaviour
 temp = np.random.rand(1, 1, 1024, 1)
 temp1 = np.random.rand(1, 1, 1024, 256)
+temp1 = temp1 / np.sum(temp1, axis=2, keepdims=True)
 
 
 
 with tf.Session() as sess:
+
+    i = 0
 
     # Create a single Wavenet structure
     example = Wavenet(filter_width=3,
@@ -34,18 +33,11 @@ with tf.Session() as sess:
 
     sess.run(tf.global_variables_initializer())
 
-    a = sess.run(example.loss, feed_dict={example.inputs: file, example.targets: temp1})
-    print("Error is: ", a)
-
-"""
-Loop for training
-
-
     while True:
-        error = sess.run(example.train())
+        a = sess.run([example.loss, example.train_step], feed_dict={example.inputs: file, example.targets: targets})
+        print("Error_{0} is: ".format(i), a)
+        i += 1
 
-        print(error)
-
-        if error < 0.5:
+        if a[0] < 0.01:
             break
-"""
+
