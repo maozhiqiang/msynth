f20d9d8c9f9cf22e49ba4b6a6e3c13561a9a3438
ws260621690 <william.starnaud@gmail.com>
2018-02-24 18:17:52 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -8,16 +8,13 @@ file = mu_law(file, 255)[:1024]
 
 shape = file.shape
 
-#Reshape input to be used by dilated convolution layer
+# Reshape input to be used by dilated convolution layer
 file = file.reshape([1, 1, shape[0], 1])
 
 # get probability distribution for the labels. Used in cross-entropy loss during training
-#targets = get_labels(file, 256) this is for categorical
+# targets = get_labels(file, 256) this is for categorical
 # targets = file * 128 + 128 this will be used later by the student Wavenet
-targets = np.ones(shape=[1,1,1024,1], dtype=np.float32)
-
-
-
+targets = np.ones(shape=[1, 1, 1024, 1], dtype=np.float32)
 
 with tf.Session() as sess:
 
@@ -25,17 +22,26 @@ with tf.Session() as sess:
 
     # Create a single Wavenet structure
     example = TeacherWavenet(filter_width=3,
-                      hidden_units=64,
-                      output_classes=1,
-                      training=True)
+                             hidden_units=64,
+                             output_classes=1,
+                             training=True)
 
     sess.run(tf.global_variables_initializer())
 
+    # Saver to save the model after training
+    saver = tf.train.Saver()
 
     while True:
-        error, _ = sess.run([example.loss, example.train_step], feed_dict={example.inputs: file, example.targets: targets})
+        error, _ = sess.run([example.loss, example.train_step],
+                            feed_dict={example.inputs: file, example.targets: targets})
         print("Error_{0} is: ".format(i), error)
-        i+=1
+        i += 1
 
         if error < 0.1:
+
+            # Default location where the model will be saved. Can change it to anything
+            save_path = saver.save(sess, "/tmp/ParallelWavenet.ckpt")
+
+            print("Model saved in {0}".format(save_path))
+
             break
