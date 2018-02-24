a5c942de772380d22fcce11c45b75e04632a45a4
ws260621690 <william.starnaud@gmail.com>
2018-02-24 17:32:33 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
-import tensorflow as tf, numpy as np, pyo, librosa
-
+import tensorflow as tf
+import numpy as np
+import pyo
+import librosa
 
 
 """
@@ -20,47 +22,48 @@ Args:
 Returns:
     y: tensor of shape [batchsize, 1, out_width, out_channel]
 """
+
+
 def dilated_conv(x,
                  name,
                  filter_width,
                  out_channel,
                  dilation=1,
-                 padding="SAME"):
+                 padding="SAME",
+                 trainable=True):
 
-    #check if height of input is 1
+    # check if height of input is 1
     with tf.variable_scope(name):
 
         assert x.shape[1] == 1
         assert name
 
-        print("X: ", x.shape) # Used to test shapes. Remove after
+        print("X: ", x.shape)  # Used to test shapes. Remove after
 
         batch_size, _, time, in_channel = x.shape
 
         w = tf.get_variable(name="w",
                             shape=[1, filter_width, in_channel,  out_channel],
                             dtype=x.dtype,
-                            initializer=tf.random_normal_initializer())
+                            initializer=tf.random_normal_initializer(),
+                            trainable=trainable)
 
         y = tf.nn.atrous_conv2d(value=x,
-                         filters=w,
-                         padding=padding,
-                         rate=dilation)
+                                filters=w,
+                                padding=padding,
+                                rate=dilation)
 
         _, y_2, y_3, _ = y.shape
 
         b = tf.get_variable(name="b",
                             shape=[1, y_2, y_3, out_channel],
                             dtype=x.dtype,
-                            initializer=tf.random_normal_initializer())
+                            initializer=tf.random_normal_initializer(),
+                            trainable=trainable)
 
-        print("Y: ", y.shape) # Used to test shapes. Remove after
+        print("Y: ", y.shape)  # Used to test shapes. Remove after
 
     return y + b
-
-
-
-
 
 
 """
@@ -75,13 +78,14 @@ Args:
 Returns:
     y: tensor (or numpy array???) with the same shape as input with mu-law values
 """
+
+
 def mu_law(x, mu):
 
-    #normalize input between 1 and -1
+    # normalize input between 1 and -1
     x_norm = x
 
     return np.sign(x_norm) * np.log(1 + np.abs(x_norm) * mu) / np.log(1 + mu)
-
 
 
 """
@@ -96,13 +100,13 @@ Args:
 Returns:
     y: the inverse mu-law of x
 """
+
+
 def inv_mu_law(x, mu):
 
     y = np.sign(x) * (np.sign(x) * np.log(1 + mu) * np.exp(x) - 1)
 
     return y * mu / 2
-
-
 
 
 """
@@ -119,10 +123,11 @@ Returns:
     
 *** For now this function uses librosa --> implement using pyo ***
 """
+
+
 def load_audio(file_path, sample_rate):
 
     return librosa.load(file_path, sample_rate)
-
 
 
 """
@@ -138,10 +143,10 @@ Returns:
     
 *** For now thus function uses librosa --> implement using pyo
 """
+
+
 def load_batch(path, sample_rate):
     pass
-
-
 
 
 """
@@ -151,6 +156,8 @@ Args:
     
 
 """
+
+
 def get_labels(x, bins):
 
     y = np.int32(np.floor(x * bins))
@@ -161,3 +168,41 @@ def get_labels(x, bins):
     out[:, :, np.arange(y.shape[0]), y] = 1
 
     return out
+
+
+"""
+Function that returns an operator that computes the discrete log mixture to be used instead 
+of the categorial distribution as it is more tractable
+
+Args:
+    x: input tensor
+    
+    shape: shape of input tensor that will be fed at run time
+    
+    params: list of parameter tuples (location, scale) to be used in mixture model
+    
+    max: max possible value of discrete input x
+    
+    min: min possbile value of discrete input x
+     
+Returns:
+    op: operator that can compute the discretized prob of an input x
+    
+*** Change the +/- 0.5 tu upper bound / lower bound, add # bits???Remove shape?????***
+"""
+
+
+def disc_log_mixt(x, shape, params, max, min):
+
+    func = 0
+
+    upper = tf.where(x == max, x + np.inf, x + 0.5)
+
+    lower = tf.where(x == min, x - np.inf, x - 0.5)
+
+    for m, s in params:
+
+        func += tf.sigmoid((upper - m) / s) - tf.sigmoid((lower - m) / s)
+
+        print(func)
+    return func
