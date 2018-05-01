6f34d3eead54ad792b7debf6d275502aef487293
ws260621690 <william.starnaud@gmail.com>
2018-04-30 21:31:33 -0400

    Update gen.py

--- gen.py
+++ gen.py
@@ -2,48 +2,66 @@ from model import *
 
 sess = tf.Session()
 
-def restore(sess, shape):
+
+def restore(session, shape):
 
     # create IAF and load previously trained weights
-    iaf = IAF(inputs=tf.placeholder(dtype=tf.float32, shape=shape),
-              flows=1,
-              filter_width=3,
-              hidden_units=2,
-              output_classes=2,
-              training=False)
+    flow = IAF(inputs=tf.placeholder(dtype=tf.float32, shape=shape),
+               cond=tf.placeholder(dtype=tf.float32, shape=shape),
+               teacher=None,
+               flows=1,
+               filter_width=3,
+               hidden_units=64,
+               output_classes=2,
+               nb_mixtures=2,
+               training=False)
 
     load = tf.train.Saver()
 
-    load.restore(sess, "/tmp/IAF.ckpt")
+    load.restore(session, "/tmp/IAF.ckpt")
 
     print("Model restored")
 
-    return iaf
+    return flow
 
 
-def generate(sess, iaf):
+def generate(session, flow, cond):
 
     # generate random noise input
     gen = np.random.logistic(0., 1., [32, 1, 10, 1])
 
-    outputs = sess.run(iaf.outputs,
-                       feed_dict={iaf.inputs: gen})
+    gen_audio = session.run(flow.outputs,
+                            feed_dict={flow.inputs: gen, flow.cond: cond})
 
-    return outputs
+    gen_audio = inv_mu_law(gen_audio, 65535.)
+
+    return gen_audio
 
 
-iaf = restore(sess, [32, 1, 10, 1])
+def main():
 
-outputs = generate(sess, iaf)
+    iaf = restore(sess, [32, 1, 10, 1])
 
-for i in range(9999):
+    gen_block = generate(sess, iaf, np.zeros([32, 1, 10, 1], np.float32))
 
-    outputs = np.concatenate([outputs, generate(sess, iaf)], axis=2)
+    print(gen_block)
 
-    # outputs = outputs / 32767.5 - 1.
+    outputs = gen_block
 
-file = outputs.reshape([outputs.shape[0], outputs.shape[2]])
+    for i in range(1):
 
-for i in range(2):
+        gen_block = generate(sess, iaf, gen_block)
 
-    save_audio("/tmp/gen_file_{0}.wav".format(i), file[i], 44100)
+        outputs = np.concatenate([outputs, gen_block], axis=2)
+
+    print("Generated audio files")
+
+    file = outputs.reshape([outputs.shape[0], outputs.shape[2]])
+
+    for i in range(2):
+
+        save_audio("/tmp/gen_file_{0}.wav".format(i), file[i], 44100)
+
+
+if __name__ == "__main__":
+    main()
